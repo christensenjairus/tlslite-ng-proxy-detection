b603b3be6562a5b9ba39b1dc67fe936f38a854cb
Jairus Christensen <christensenjairus@gmail.com>
2022-10-25 08:23:47 -0600

    added headers to stats

--- tests/statsOfRttLog.sh
+++ tests/statsOfRttLog.sh
@@ -1,1 +1,14 @@
-cat rtt.log | awk '{print $5}' | datamash min 1 max 1 median 1 mean 1 svar 1 q1 1 q3 1
+echo -n "Minimum: "
+cat rtt.log | awk '{print $5}' | datamash min 1
+echo -n "Maximum: "
+cat rtt.log | awk '{print $5}' | datamash max 1
+echo -n "Median: "
+cat rtt.log | awk '{print $5}' | datamash median 1
+echo -n "Mean: "
+cat rtt.log | awk '{print $5}' | datamash mean 1
+echo -n "Sample Variance: "
+cat rtt.log | awk '{print $5}' | datamash svar 1
+echo -n "Quartile 1: "
+cat rtt.log | awk '{print $5}' | datamash q1 1
+echo -n "Quartile 3: "
+cat rtt.log | awk '{print $5}' | datamash q3 1
